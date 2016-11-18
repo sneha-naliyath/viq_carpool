a776484055b9170b6e510c5bb3a3d5321a92c918
sneha-naliyath <word2sneha@gmail.com>
2016-11-18 12:49:17 +0530

    re choice

--- app/src/main/java/com/example/snehanaliyath/carpooling/Choice.java
+++ app/src/main/java/com/example/snehanaliyath/carpooling/Choice.java
@@ -7,24 +7,32 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class Choice extends AppCompatActivity {
+public class Choice extends AppCompatActivity  implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button5 = (Button) findViewById(R.id.button5);
 
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+        button3.setOnClickListener(this);
+        button5.setOnClickListener(this);
     }
-    public void offer(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, OfferRide.class);
 
-        startActivity(intent);
+    public void onClick(View v) {
 
+        switch (v.getId())
+        {
+            case R.id.button3: Intent intent = new Intent(this, OfferRide.class);
+                startActivity(intent);
+                break;
+            case R.id.button5: Intent intent1 = new Intent(this, FindRide.class);
+                startActivity(intent1);
+                break;
+
+        }
     }
 }
